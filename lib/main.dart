f2ac205030944dfb240e372561aa2a3b4b1a7aa1
RAUL_JAHZIEL_DELGADO_HERNANDEZ <a.20308051280597@cbtis128.edu.mx>
2023-03-22 18:47:58 +0000

    Drawer_6

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,11 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  static const String _title = 'Drawer Radical sisis';
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
@@ -18,6 +17,8 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+final GlobalKey<ScaffoldState> _key = GlobalKey(); // Create a key
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -30,79 +31,95 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('*+._ Drawer Raul _.+*'),
-        backgroundColor: const Color(0xff623dc8),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const UserAccountsDrawerHeader(
-              decoration: BoxDecoration(color: const Color(0xff3a277e)),
-              accountName: Text(
-                "Raul Jahziel",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              accountEmail: Text(
-                "a.20308051280597@cbtis128.edu.mx",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              currentAccountPicture: FlutterLogo(),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-              ),
-              title: const Text('Pagina 1'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.train,
-              ),
-              title: const Text('Pagina 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.balance,
-              ),
-              title: const Text('Pagina 3'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            AboutListTile(
-              // <-- SEE HERE
-              icon: Icon(
-                Icons.info,
-              ),
-              child: Text('About app'),
-              applicationIcon: Icon(
-                Icons.local_play,
-              ),
-              applicationName: 'My Cool App',
-              applicationVersion: '1.0.25',
-              applicationLegalese: '© 2019 Company',
-              aboutBoxChildren: [
-                ///Content goes here...
-              ],
-            )
-          ],
+        key: _key,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('*+._ Drawer Raul _.+*'),
+          backgroundColor: const Color(0xff623dc8),
         ),
-      ),
-      body: Center(),
-    );
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: const Color(0xff3a277e)),
+                accountName: Text(
+                  "Raul Jahziel",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                accountEmail: Text(
+                  "a.20308051280597@cbtis128.edu.mx",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                currentAccountPicture: FlutterLogo(),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.home,
+                ),
+                title: const Text('Pagina 1'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.train,
+                ),
+                title: const Text('Pagina 2'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.balance,
+                ),
+                title: const Text('Pagina 3'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              AboutListTile(
+                // <-- SEE HERE
+                icon: Icon(
+                  Icons.info,
+                ),
+                child: Text('About app'),
+                applicationIcon: Icon(
+                  Icons.local_play,
+                ),
+                applicationName: 'My Cool App',
+                applicationVersion: '1.0.25',
+                applicationLegalese: '© 2019 Company',
+                aboutBoxChildren: [
+                  ///Content goes here...
+                ],
+              )
+            ],
+          ),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _key.currentState!.openDrawer(); //<-- SEE HERE
+              },
+              child: const Text(
+                'Elevated Button 1',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ],
+        )));
   }
 }
